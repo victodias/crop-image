aa33a44d1638b838999c14144ffb7bd79db58a8a
Victor Dias <victoroliveirad96@gmail.com>
2020-09-30 00:12:11 -0300

    Implement canvas and download of cropped image

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 const photoFile = document.getElementById('photo-file')
-let image = document.getElementById('photo-preview')
+let photoPreview = document.getElementById('photo-preview')
+let image;
+let photoName;
 
 // Select and preview image
 document.getElementById('select-image').onclick = function () {
@@ -9,11 +11,14 @@ document.getElementById('select-image').onclick = function () {
 window.addEventListener('DOMContentLoaded', () => {
   photoFile.addEventListener('change', () => {
     let file = photoFile.files.item(0)
+    photoName = file.name
     // ler arquivo
     let reader = new FileReader()
     reader.readAsDataURL(file)
     reader.onload = function (event) {
+      image = new Image()
       image.src = event.target.result // resultado do reader - imagem carregada
+      image.onload = onLoadImage
     }
   })
 })
@@ -57,14 +62,92 @@ const events = {
       selection.style.height = (endY - startY) + 'px'
     }
   },
-  mouseup(event) { 
+  mouseup(event) {
     startSelection = false
 
     relativeEndX = event.layerX;
     relativeEndY = event.layerY;
+    cropButton.style.display = 'initial'
   }
 }
 
 Object.keys(events).forEach(eventName => {
-  image.addEventListener(eventName, events[eventName])
+  photoPreview.addEventListener(eventName, events[eventName])
 })
+
+// Canvas
+let canvas = document.createElement('canvas')
+let ctx = canvas.getContext('2d')
+
+function onLoadImage() {
+  const { width, height } = image
+  canvas.width = width
+  canvas.height = height
+
+  // Limpar contexto do canvas
+  ctx.clearRect(0, 0, width, height)
+
+  // Desenhar a imagem no contexto
+  ctx.drawImage(image, 0, 0)
+
+  photoPreview.src = canvas.toDataURL()
+}
+
+// Cortar imagem
+const cropButton = document.getElementById('crop-image')
+cropButton.onclick = () => {
+  const { width: imgW, height: imgH } = image
+  const { width: previewW, height: previewH } = photoPreview
+
+  const [widthFactor, heightFactor] = [
+    +(imgW / previewW),
+    +(imgH / previewH)
+  ]
+
+  const [selectionWidth, selectionHeight] = [
+    +selection.style.width.replace('px', ''),
+    +selection.style.height.replace('px', '')
+  ]
+
+  const [croppedWidth, croppedHeight] = [
+    +(selectionWidth * widthFactor),
+    +(selectionHeight * heightFactor)
+  ]
+
+  const [actualX, actualY] = [
+    +(relativeStartX * widthFactor),
+    +(relativeStartY * heightFactor)
+  ]
+
+  // Pegar do ctx a imagem cortada
+  const croppedImage = ctx.getImageData(actualX, actualY, croppedWidth, croppedHeight)
+
+  // Limpar contexto do canvas
+  ctx.clearRect(0, 0, ctx.width, ctx.height)
+
+  // Ajuste de proporções
+  image.width = canvas.width = croppedWidth;
+  image.height = canvas.height = croppedHeight;
+
+  // Adicionar a imagem cortada ao contexto do canvas
+  ctx.putImageData(croppedImage, 0, 0)
+
+  // Esconder a ferramenta de seleção
+  selection.style.display = 'none'
+
+  // Atualizar o preview da imagem, transformando os dados da imagem em uma URL, adicionando ao src da imagem
+  photoPreview.src = canvas.toDataURL()
+
+  // mostrar botão de download
+  downloadButton.style.display = 'initial'
+}
+
+// Download 
+const downloadButton = document.getElementById('download')
+
+downloadButton.onclick = function () {
+  const a = document.createElement('a')
+  a.download = photoName + '-cropped.png'
+  a.href = canvas.toDataURL()
+  a.click()
+}
